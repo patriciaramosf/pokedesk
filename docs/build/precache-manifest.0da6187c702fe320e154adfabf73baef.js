50f5d83944baea1f790ca9a67c481807f64b9769
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-07 11:32:54 +0200

    changes

---
+++ docs/build/precache-manifest.0da6187c702fe320e154adfabf73baef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659224fb128b6342430135ba5d416a5b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94b1e5897fe85a4df4f5",
+    "url": "./static/css/main.b2a709b7.chunk.css"
+  },
+  {
+    "revision": "ff6a424fa5759d9c9f6c",
+    "url": "./static/js/2.c5fd04c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c5fd04c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b1e5897fe85a4df4f5",
+    "url": "./static/js/main.98154c66.chunk.js"
+  },
+  {
+    "revision": "c330c060871788d8de18",
+    "url": "./static/js/runtime-main.eccfa354.js"
+  }
+]);
