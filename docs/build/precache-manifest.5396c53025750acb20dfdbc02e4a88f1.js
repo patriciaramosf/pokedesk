9df441c61afc2c39bad767b75a27cfb1b9f63da1
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-18 20:46:58 +0200

    final pokedesk with no api

---
+++ docs/build/precache-manifest.5396c53025750acb20dfdbc02e4a88f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce7229bc26c8c8223e0297435b90015",
+    "url": "./index.html"
+  },
+  {
+    "revision": "562dd465176bacfacd0a",
+    "url": "./static/css/main.bfc600b4.chunk.css"
+  },
+  {
+    "revision": "15360c9962f21f8bd543",
+    "url": "./static/js/2.a78bb82c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a78bb82c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562dd465176bacfacd0a",
+    "url": "./static/js/main.da4c1bb8.chunk.js"
+  },
+  {
+    "revision": "c330c060871788d8de18",
+    "url": "./static/js/runtime-main.eccfa354.js"
+  },
+  {
+    "revision": "dfadb7aa68741e5b5e3c5553942abbbb",
+    "url": "./static/media/pokemon.dfadb7aa.png"
+  }
+]);
