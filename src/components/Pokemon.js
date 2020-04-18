f7a50b1b7b72a50ba3ebbcbf0d1003dc71a255d7
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-18 16:43:00 +0200

    changing symbols

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -13,7 +13,8 @@ const Pokemon = (props) =>{
             <div className="Pokemon_types">
                 {props.pokeTypes.map((myType, index) =>
                 <div className="general">
-                 <div className="circle"><i className="fas fa-fire-alt"></i></div>
+                 <div className="circle"><i className={`fas ${myType=== "fire" ? "fa-fire-alt" : myType=== "water" ? "fa-tint" : myType === "grass" ? "fa-leaf" : "fa-bug"}`}></i>
+                 </div>
                 <p key={index} className="myTwoTypes">
                     {myType}
                 </p>
@@ -21,7 +22,10 @@ const Pokemon = (props) =>{
                 </div>
                 )} 
             </div>
-
+            {/*
+            flying <i class="fas fa-dove"></i>
+            poison <i class="fas fa-skull-crossbones"></i>
+             */}
         </li>
     )
 }
