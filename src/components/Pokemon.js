93010a300a6b55591da5c565cce35ab0ed624c83
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-18 15:27:13 +0200

    last changes sass

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,29 +5,21 @@ const Pokemon = (props) =>{
         <li className="Pokemon__container">
              <div className="Pokemon_name">
                 <h2>{props.pokeName}</h2>
+                <div className="magic"><i class="fas fa-magic"></i></div>
             </div>
             <div className="Pokemon_photo">
                 <img className="photo" src={props.pokeImage} alt={props.pokeName}/>
             </div>
             <div className="Pokemon_types">
-                <div className="type1">
-                <div className="circle"><i className="fas fa-fire-alt"></i></div>
                 {props.pokeTypes.map((myType, index) =>
+                <div className="general">
+                 <div className="circle"><i className="fas fa-fire-alt"></i></div>
                 <p key={index} className="myTwoTypes">
                     {myType}
                 </p>
-                )}
-                <p className="number">30</p>
+                <p className="number">8</p>
                 </div>
-                <div className="type2">
-                <div className="circle"><i className="fas fa-fire-alt"></i></div>
-                {props.pokeTypes.map((myType, index) =>
-                <p key={index} className="myTwoTypes">
-                    {myType}
-                </p>
-                )}
-                <p className="number">60</p>
-                </div>
+                )} 
             </div>
 
         </li>
