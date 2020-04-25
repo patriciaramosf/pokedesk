ebd267ad55815b900baaf9026f961fc6ec3bade9
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-25 11:28:11 +0200

    all errors fixed

---
+++ docs/precache-manifest.0b5b357b8fe4b8af46b0726f75818401.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b43be7d18a54287919a77569d7efc6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d349374663dbeb038ff4",
+    "url": "./static/css/main.78fbc4a9.chunk.css"
+  },
+  {
+    "revision": "d077c57383f5320aa159",
+    "url": "./static/js/2.aa00ef52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.aa00ef52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d349374663dbeb038ff4",
+    "url": "./static/js/main.df2b35da.chunk.js"
+  },
+  {
+    "revision": "c330c060871788d8de18",
+    "url": "./static/js/runtime-main.eccfa354.js"
+  },
+  {
+    "revision": "dfadb7aa68741e5b5e3c5553942abbbb",
+    "url": "./static/media/pokeball.dfadb7aa.png"
+  },
+  {
+    "revision": "1af3e2a674657adb60e7621b90736bcb",
+    "url": "./static/media/pokemon.1af3e2a6.png"
+  }
+]);
